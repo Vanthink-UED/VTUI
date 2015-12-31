2fcc8d68a2e2a41d389b38631e10735ce34daa15
JackPu <kakashjack@gmail.com>
2015-12-31 14:58:14 +0800

    add vtui.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 var gulp = require('gulp');
-//var minifycss = require('gulp-minify-css');
+var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var plumber = require('gulp-plumber');
 var less = require('gulp-less');
 var path = require('path');
 var gutil = require('gulp-util');
+var rename = require('gulp-rename');
 var path2 = {
     'css':['./dist/*.css'],
     'js':['./public/js/**/*.js','!./js/city.min.js','!./js/lib/jquery.validVal-customValidations.js']
 };
 
 gulp.task('less', function () {
-  return gulp.src(['./public/less/style.less'])
+  return gulp.src(['./less/vtui.less'])
         .pipe(plumber(function(error) {
             gutil.log(gutil.colors.red(error.message));
             gutil.beep();
@@ -19,8 +20,18 @@ gulp.task('less', function () {
         }))
        .pipe(less())
 
-    .pipe(minifycss())
-    .pipe(gulp.dest('./css'));
+    //.pipe(minifycss())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('minify', function () {
+    gulp.src('./dist/vtui.css')
+        .pipe(minifycss())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./dist'))
+    ;
 });
 
 
@@ -28,9 +39,8 @@ gulp.task('less', function () {
 gulp.task('watch',function(){
     gulp.watch('./less/**/*.less', function(){
         gulp.run('less');
+        gulp.run('minify');
     });
-        //gulp.watch(path['css'],['minifycss']);
-        //gulp.watch(path['js'],['uglifyjs']);
 
 });
 
